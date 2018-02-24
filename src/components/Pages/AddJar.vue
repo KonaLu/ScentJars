d1ad455ece33d3eeacf8f32714fa120c096d162e
unknown <edbarr@gmail.com>
2018-02-24 11:53:00 -0500

    Added more variable to input page. Added button (not functional), added v-model to each variable.

--- src/components/Pages/AddJar.vue
+++ src/components/Pages/AddJar.vue
@@ -23,6 +23,7 @@
               <v-text-field name="FirstName"
               label="First Name"
               id="FirstName"
+              v-model="FirstName"
               required>
               </v-text-field>
             </v-flex>
@@ -30,6 +31,7 @@
               <v-text-field name="LastName"
               label="Last Name"
               id="LastName"
+              v-model="LastName"
               required>
               </v-text-field>
             </v-flex>
@@ -37,6 +39,7 @@
               <v-text-field name="NickName"
               label="Name to Call"
               id="NickName"
+              v-model="NickName"
               >
               </v-text-field>
             </v-flex>
@@ -46,6 +49,7 @@
               <v-text-field name="SteetAddress"
               label="Steet Address"
               id="SteetAddress"
+              v-model="StreetAddress"
               required>
               </v-text-field>
             </v-flex>
@@ -53,6 +57,7 @@
               <v-text-field name="City"
               label="City"
               id="City"
+              v-model="City"
               required>
               </v-text-field>
             </v-flex>
@@ -62,6 +67,7 @@
               <v-text-field name="HomePhone"
               label="Home Phone"
               id="HomePhone"
+              v-model="HomePhone"
               >
               </v-text-field>
             </v-flex>
@@ -69,6 +75,7 @@
               <v-text-field name="CellPhone"
               label="Cell Phone"
               id="CellPhone"
+              v-model="CellPhone"
               >
               </v-text-field>
             </v-flex>
@@ -86,6 +93,7 @@
               <v-text-field name="CollectionDate"
               label="Collection Date"
               id="CollectionDate"
+              v-model="CollectionDate"
               required
               >
               </v-text-field>
@@ -94,6 +102,7 @@
               <v-text-field name="Collector"
               label="Collector"
               id="Collector"
+              v-model="Collector"
               required
               >
               </v-text-field>
@@ -111,6 +120,7 @@
               <v-text-field name="Height"
               label="Height"
               id="Height"
+              v-model="Height"
               >
               </v-text-field>
             </v-flex>
@@ -118,6 +128,7 @@
               <v-text-field name="Weight"
               label="Weight"
               id="Weight"
+              v-model="Weight"
               >
               </v-text-field>
             </v-flex>
@@ -127,6 +138,7 @@
               <v-text-field name="Sex"
               label="Sex"
               id="Sex"
+              v-model="Sex"
               >
               </v-text-field>
             </v-flex>
@@ -134,6 +146,7 @@
               <v-text-field name="Race"
               label="Race"
               id="Race"
+              v-model="Race"
               >
               </v-text-field>
             </v-flex>
@@ -143,6 +156,7 @@
               <v-text-field name="HairColor"
               label="Hair Color"
               id="HairColor"
+              v-model="HairColor"
               >
               </v-text-field>
             </v-flex>
@@ -150,6 +164,7 @@
               <v-text-field name="HairStyle"
               label="Hair Style"
               id="HairStyle"
+              v-model="HairStyle"
               >
               </v-text-field>
             </v-flex>
@@ -159,6 +174,8 @@
               <v-text-field name="Marks"
               label="Distinguishing Marks, Comments"
               id="Marks"
+              v-model="Marks"
+              multi-line
               >
               </v-text-field>
             </v-flex>
@@ -166,17 +183,26 @@
           <v-layout row>
             <v-flex xs12 sm6 offset-sm3>
               <v-text-field name="Approachable"
-              label="Approachable?"
+              label="Approachable (Y/N)"
               id="Approachable"
+              v-model="Approachable"
               >
               </v-text-field>
             </v-flex>
             <v-flex xs12 sm6 offset-sm3>
               <v-text-field name="Dogs"
-              label="Fear of Dogs?"
+              label="Fear of Dogs (Y/N)"
               id="Dogs"
+              v-model="Dogs"
               >
               </v-text-field>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-btn class="primary">
+                Add Data
+              </v-btn>
             </v-flex>
           </v-layout>
  
@@ -185,3 +211,28 @@
     </v-layout>
   </v-container>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      FirstName: '',
+      LastName: '',
+      NickName: '',
+      StreetAddress: '',
+      City: '',
+      HomePhone: '',
+      CellPhone: '',
+      Height: '',
+      Weight: '',
+      Sex: '',
+      Race: '',
+      HairColor: '',
+      HairStyle: '',
+      Marks: '',
+      Approachable: '',
+      Dogs: ''
+    }
+  }
+}
+</script>
